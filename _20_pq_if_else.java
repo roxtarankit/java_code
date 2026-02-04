5a762edbee50b90efc16c4e4875aa3b854af8d0d
roxtarankit <ankitsinghas12343@gmail.com>
2026-02-04 18:02:04 +0530

    version5

---
+++ _20_pq_if_else.java
@@ -0,0 +1,26 @@
+// Question1:
+// Write a Java program to get a number from the user and print whether it is positive or negative.
+
+import java.util.*;
+
+public class _20_pq_if_else{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int num=sc.nextInt();
+
+        if(num>0)
+        {
+            System.out.println("Number is Positive.");
+        }
+        else if(num<0)
+        {
+            System.out.println("Number is negative");
+        }
+        else{
+            System.out.println("Number is Zero");
+        }
+    }
+}
---
+++ _21_pq_if_else.java
@@ -0,0 +1,18 @@
+// Question2: 
+// Finish the following code so that it prints You have a fever if your temperature is above 100 and otherwise prints You don't have a fever
+
+public class _21_pq_if_else{
+    public static void main(String args[]){
+
+        double temp=10.5;
+
+        if(temp>100)
+        {
+            System.out.println("You have a fever.");
+        }
+        else
+        {
+            System.out.println("You don't have fever.");
+        }
+    }
+}
